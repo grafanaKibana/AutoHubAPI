cc497c896eff269730c720a321343091ac95c666
aldecode <reshetnik.nikita@gmail.com>
2021-10-02 20:06:16 +0300

    Implemented AutoMapper to CarService.cs

--- AutoHub.BLL/Services/CarService.cs
+++ AutoHub.BLL/Services/CarService.cs
@@ -2,22 +2,30 @@
 using System.Linq;
 using AutoHub.BLL.Interfaces;
 using AutoHub.BLL.Models;
+using AutoHub.DAL.Entities;
 using AutoHub.DAL.Interfaces;
+using AutoMapper;
 
 namespace AutoHub.BLL.Services
 {
     public class CarService : ICarService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly Mapper _mapper;
 
         public CarService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+
+            var mapperConfig = new MapperConfiguration(cfg => cfg.CreateMap<Car, CarModel>());
+            _mapper = new Mapper(mapperConfig);
         }
 
         public IEnumerable<CarModel> GetAll()
         {
-            return _unitOfWork.Cars.GetAll().Select(car =>
+            return _mapper.Map<IEnumerable<CarModel>>(_unitOfWork.Cars.GetAll());
+
+            /*return unitOfWork.Cars.GetAll().Select(car =>
                 new CarModel
                 {
                     CarId = car.CarId,
@@ -30,17 +38,19 @@ namespace AutoHub.BLL.Services
                     VIN = car.VIN,
                     Mileage = car.Mileage,
                     SellingPrice = car.SellingPrice
-                });
+                });*/
         }
 
         public CarModel GetById(int id)
         {
             var car = _unitOfWork.Cars.GetById(id);
 
-            if (car == null) 
+            if (car == null)
                 return null;
 
-            return new CarModel
+            return _mapper.Map<CarModel>(car);
+
+            /*return new CarModel
             {
                 CarId = car.CarId,
                 ImgUrl = car.ImgUrl,
@@ -52,7 +62,7 @@ namespace AutoHub.BLL.Services
                 VIN = car.VIN,
                 Mileage = car.Mileage,
                 SellingPrice = car.SellingPrice
-            };
+            };*/
         }
     }
 }
