1b518e3968117a86757df92085212686bf20dc9a
aldecode <reshetnik.nikita@gmail.com>
2021-10-30 22:04:53 +0300

    Updated BidService.cs

--- AutoHub.BLL/Interfaces/IBidService.cs
+++ AutoHub.BLL/Interfaces/IBidService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AutoHub.BLL.DTOs.BidDTOs;
 
 namespace AutoHub.BLL.Interfaces
@@ -5,6 +6,10 @@ namespace AutoHub.BLL.Interfaces
     public interface IBidService
     {
         BidResponseDTO GetById(int bidId);
+        IEnumerable<BidResponseDTO> GetUserBids(int userId);
+        BidResponseDTO GetUserBidById(int userId, int bidId);
+        IEnumerable<BidResponseDTO> GetLotBids(int lotId);
+        BidResponseDTO GetLotBidById(int lotId, int bidId);
         void CreateBid(BidCreateRequestDTO createBidDTO);
     }
 }
--- AutoHub.BLL/Services/BidService.cs
+++ AutoHub.BLL/Services/BidService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using AutoHub.BLL.DTOs.BidDTOs;
 using AutoHub.BLL.Interfaces;
 using AutoHub.DAL.Entities;
@@ -21,6 +22,34 @@ namespace AutoHub.BLL.Services
         public BidResponseDTO GetById(int bidId)
         {
             var bid = _unitOfWork.Bids.GetById(bidId);
+            var mappedBid = _mapper.Map<BidResponseDTO>(bid);
+            return mappedBid;
+        }
+
+        public IEnumerable<BidResponseDTO> GetUserBids(int userId)
+        {
+            var bids = _unitOfWork.Bids.Find(bid => bid.UserId == userId);
+            var mappedBids = _mapper.Map<IEnumerable<BidResponseDTO>>(bids);
+            return mappedBids;
+        }
+
+        public BidResponseDTO GetUserBidById(int userId, int bidId)
+        {
+            var bid = _unitOfWork.Bids.Find(bid => bid.UserId == bidId && bid.BidId == bidId);
+            var mappedBid = _mapper.Map<BidResponseDTO>(bid);
+            return mappedBid;
+        }
+
+        public IEnumerable<BidResponseDTO> GetLotBids(int lotId)
+        {
+            var bids = _unitOfWork.Bids.Find(bid => bid.LotId == lotId);
+            var mappedBids = _mapper.Map<IEnumerable<BidResponseDTO>>(bids);
+            return mappedBids;
+        }
+
+        public BidResponseDTO GetLotBidById(int lotId, int bidId)
+        {
+            var bid = _unitOfWork.Bids.Find(bid => bid.LotId == lotId && bid.BidId == bidId);
             var mappedBid = _mapper.Map<BidResponseDTO>(bid);
             return mappedBid;
         }
