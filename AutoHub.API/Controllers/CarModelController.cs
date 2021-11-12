19118af43da73f7e9f2b3e3bcbc9b568d21537b0
aldecode <reshetnik.nikita@gmail.com>
2021-11-12 21:42:14 +0200

    Done with CarModelController Unit-tests

--- AutoHub.API/Controllers/CarModelController.cs
+++ AutoHub.API/Controllers/CarModelController.cs
@@ -25,7 +25,7 @@ namespace AutoHub.API.Controllers
 
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<CarModelResponseModel>), StatusCodes.Status200OK)]
-        public IActionResult GetAll()
+        public IActionResult GetAllCarModels()
         {
             try
             {
